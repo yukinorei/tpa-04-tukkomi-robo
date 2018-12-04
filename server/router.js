96f729ca72693a9252b694b40407774b9a2d6361
yukinorei <>
2018-12-05 00:43:52 +0900

    APIルーターを実装

--- server/router.js
+++ server/router.js
@@ -1,18 +1,17 @@
 const express = require('express');
+const getRandomTukkomi = require('./get-random-tukkomi.js');
 
 const tukkomiRoboRouter = express.Router();
 
 tukkomiRoboRouter.get('/', (req, res) => {
   const { phrase } = req.query;
-  console.log('phrase = ', phrase);
-
-  //
-  // TODO
-  //
-  // if phrase が空っぽだったらエラーメッセージを送ろう。
-  // else phrase にツッコミを追加して送ろう。 (HINT: get-random-tukkomi.js が役に立つかも）
-
-  res.send('ツッコミタイム');
+  if (!phrase) {
+    const errorMessage = '何も言わんの？';
+    res.send(errorMessage);
+  } else {
+    const tukkomiMessage = `${phrase}...って、${getRandomTukkomi()}`;
+    res.send(tukkomiMessage);
+  }
 });
 
 module.exports = tukkomiRoboRouter;
